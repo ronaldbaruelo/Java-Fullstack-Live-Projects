2a082effa8d33e7b018d9deb63437046629f2d2d
Ronald Baruelo <rcbaruelo10292@gmail.com>
2021-02-28 19:57:24 +1300

    Methods

---
+++ methods/src/methods/methodsExecution.java
@@ -0,0 +1,28 @@
+package methods;
+
+public class methodsExecution {
+     
+	
+	public int multipynumbers(int a,int b) {
+		int z=a*b;
+		return z;
+	}
+
+	public static void main(String[] args) {
+		
+		methodsExecution b=new methodsExecution();
+		int ans= b.multipynumbers(10,3);
+		System.out.println("Multipilication is :"+ans);
+
+
+	}
+	
+		}
+	
+
+
+
+
+
+
+
